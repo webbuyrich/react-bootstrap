297b5ff83cf9a093d87fa3029e2226cf1a7d3ddb
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 21:48:54 -0500

    added breadcrumb, and card with image and text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { Button, Alert } from 'react-bootstrap'
+import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap'
 
 
 
@@ -11,6 +11,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">  
+        <Card className="mb-3 mt-5" style={{ color: "#000"}}>
+          <Card.Img src="https://loodibee.com/wp-content/uploads/nfl-cincinnati-bengals-team-logo.png" style={{ height: 350 }}/>
+          <Card.Body>
+            <Card.Title>Card Example</Card.Title>
+            <Card.Text>
+              This is an example of react bootstrap cards
+            </Card.Text>
+            <Button>Read More</Button>
+          </Card.Body>          
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test</Breadcrumb.Item>
+          <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+        </Breadcrumb>
         <Alert variant="success">This is a button</Alert>      
         <Button>Test Button</Button>       
       </header>
