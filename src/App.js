6021670fa9946851a6e71dd52d3f12518195ac64
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 21:03:10 -0500

    testing name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My name is Richard
         </a>
       </header>
     </div>
