ad4c0034a430c2f8be5a26fedfc5046e353be848
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 21:27:48 -0500

    imported alert and added variant to change class. primary is default. none has no bg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Button, Alert } from 'react-bootstrap'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Test Button</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My name is Richard Peterson
-        </a>
+      <header className="App-header">  
+        <Alert variant="success">This is a button</Alert>      
+        <Button>Test Button</Button>       
       </header>
     </div>
   );
