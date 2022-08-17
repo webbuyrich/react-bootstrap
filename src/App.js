084b01e71c6347368dd67229917ce830e2723754
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 22:13:17 -0500

    added form with label, control, and text. added controlId for tabbing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap'
+import { Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
 
 
 
@@ -11,7 +11,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">  
-        <Card className="mb-3 mt-5" style={{ color: "#000"}}>
+        <Form className="mb-2 mt-5">
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Example@email.com" />
+            <Form.Text className="text-muted">
+              We'll never share your address, trust us!
+            </Form.Text>
+          </Form.Group> 
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            <Button variant="secondary" type="submit">Login</Button>
+          </Form.Group>          
+        </Form>
+        <Card className="mb-3 mt-2" style={{ color: "#000"}}>
           <Card.Img src="https://loodibee.com/wp-content/uploads/nfl-cincinnati-bengals-team-logo.png" style={{ height: 350 }}/>
           <Card.Body>
             <Card.Title>Card Example</Card.Title>
